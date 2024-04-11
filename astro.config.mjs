b06495b09fb4faa88751f1cf463bc7bcb9e22820
Bartosz Skrobacki <bartosz.skrobacki@merce.com>
2024-04-11 09:50:19 +0200

    Add ssr for index file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,20 @@
 import { defineConfig, squooshImageService } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-
 import solidJs from '@astrojs/solid-js'
+
+import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), solidJs()],
-	server: { port: 3100 },
+	server: {
+		port: 3100
+	},
 	image: {
 		service: squooshImageService()
-	}
+	},
+	output: 'hybrid',
+	adapter: node({
+		mode: 'standalone'
+	})
 })
