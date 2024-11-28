a1c0aa54a04b6f40d629f3bbeaffc1c309f37625
Bartosz Skrobacki <bartosz.skrobacki@merce.com>
2024-11-28 22:53:20 +0100

    Update some informations on website

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -44,7 +44,29 @@ export default {
 				'accents-800': opacityColor('accents-800'),
 				'accents-850': opacityColor('accents-850'),
 				'accents-900': opacityColor('accents-900'),
-				'accents-950': opacityColor('accents-950')
+				'accents-950': opacityColor('accents-950'),
+				'blue-100': '#1fb6ff',
+				purple: '#7e5bef',
+				pink: '#ff49db',
+				orange: '#ff7849',
+				green: '#13ce66',
+				yellow: '#ffc82c',
+				'gray-dark': '#273444',
+				gray: '#8492a6',
+				'gray-light': '#d3dce6'
+			},
+			fontFamily: {
+				sans: ['Graphik', 'sans-serif'],
+				serif: ['Merriweather', 'serif']
+			},
+			extend: {
+				spacing: {
+					'8xl': '96rem',
+					'9xl': '128rem'
+				},
+				borderRadius: {
+					'4xl': '2rem'
+				}
 			}
 		}
 	},
