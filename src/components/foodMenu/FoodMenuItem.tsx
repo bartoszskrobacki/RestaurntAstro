4379631171328d72cac85784616e63b0e8845473
Bartosz Skrobacki <bartosz.skrobacki@merce.com>
2024-11-28 22:46:16 +0100

    Update some informations on website

--- src/components/foodMenu/FoodMenuItem.tsx
+++ src/components/foodMenu/FoodMenuItem.tsx
@@ -8,9 +8,9 @@ type Props = {
 export const MenuItem: Component<Props> = ({ foodMenuItem }) => {
 	return (
 		<div class="relative flex min-h-[3rem] flex-col items-center md:w-72">
-			<h3 class="mt-4 text-sm uppercase">{foodMenuItem?.fields.name}</h3>
-			<span class="min-h-[1rem] text-xs text-primary-200">{foodMenuItem?.fields.description}</span>
-			<span class="mb-1 text-xs">{foodMenuItem.fields.price.toFixed(2)}zł</span>
+			<h3 class="mt-4 text-sm uppercase">{foodMenuItem?.fields?.name}</h3>
+			<span class="min-h-[1rem] text-xs text-primary-200">{foodMenuItem?.fields?.description}</span>
+			<span class="mb-1 text-xs">{foodMenuItem.fields?.price.toFixed(2)}zł</span>
 			<span class="dots"></span>
 		</div>
 	)
